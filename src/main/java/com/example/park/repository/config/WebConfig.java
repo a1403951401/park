41ffc54b94194a5e907de461f7d44da9e66beac7
Martin <1403951401@qq.com>
2021-05-08 16:57:15 +0800

    [+] 添加 jwt、验证码

---
+++ src/main/java/com/example/park/repository/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.example.park.repository.config;
+
+import com.baomidou.kisso.web.interceptor.SSOSpringInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
+@ControllerAdvice
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // kisso 拦截器配置
+        registry.addInterceptor(new SSOSpringInterceptor()).addPathPatterns("/**").excludePathPatterns("/login");
+    }
+}
